0d07e0a75ae922237982252917a05cc919ab5a69
Mohammad Younes <myounes@gmail.com>
2014-03-30 11:59:45 +0300

    fix vertical scroll detection

--- jquery-scrollLock.js
+++ jquery-scrollLock.js
@@ -18,35 +18,39 @@
       });
     else
       return this.each(function () {
-        $(this).on(eventName + eventNamespace, function (event) {				
-					//allow zooming
-					if(!event.ctrlKey){
-          	var $this = $(this),
-							clientWidth  = $this.prop('clientWidth'),
-							offsetWidth  = $this.prop('offsetWidth')
-						;
-						//has vertical scroll
-						if(clientWidth < offsetWidth){
-							var scrollTop = $this.scrollTop(),
-									scrollHeight = $this.prop('scrollHeight'),
-									clientHeight = $this.prop('clientHeight'),
-									delta = event.originalEvent.wheelDelta || (-1 * event.originalEvent.detail) || (-1 * event.originalEvent.deltaY),
-									deltaY = 0
-							;
-							if(event.type == "wheel"){
-								 var ratio = $this.height()/$(window).height();
-								 deltaY = event.originalEvent.deltaY * ratio;
-							}
-							if (delta > 0 && scrollTop + deltaY <= 0 || delta < 0 && scrollTop + deltaY >= scrollHeight - clientHeight) {
-								event.stopPropagation();
-								event.preventDefault();
-								if (deltaY)
-									$this.scrollTop(scrollTop + deltaY)
-							}
-						}
-					}
+        $(this).on(eventName + eventNamespace, function (event) {
+          //allow zooming
+          if (!event.ctrlKey) {
+            var $this = $(this);
+            if (hasVerticalScroll($this)) {
+              var scrollTop = $this.scrollTop(),
+                  scrollHeight = $this.prop('scrollHeight'),
+                  clientHeight = $this.prop('clientHeight'),
+                  delta = event.originalEvent.wheelDelta || (-1 * event.originalEvent.detail) || (-1 * event.originalEvent.deltaY),
+                  deltaY = 0
+              ;
+              if (event.type == "wheel") {
+                var ratio = $this.height() / $(window).height();
+                deltaY = event.originalEvent.deltaY * ratio;
+              }
+              if (delta > 0 && scrollTop + deltaY <= 0 || delta < 0 && scrollTop + deltaY >= scrollHeight - clientHeight) {
+                event.stopPropagation();
+                event.preventDefault();
+                if (deltaY)
+                  $this.scrollTop(scrollTop + deltaY)
+              }
+            }
+          }
         });
       });
+  };
+  function hasVerticalScroll($element) {
+    var clientWidth = $element.prop('clientWidth'),
+        offsetWidth = $element.prop('offsetWidth'),
+        borderRightWidth = parseInt($element.css('border-right-width'), 10),
+        borderLeftWidth = parseInt($element.css('border-left-width'), 10)
+    ;
+    return clientWidth + borderLeftWidth + borderRightWidth < offsetWidth;
   };
   // no conflict
   $.fn.scrollLock.noConflict = function () {
