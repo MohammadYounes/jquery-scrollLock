d272f735407088c44e52ce8f2e510b95472ff051
Mohammad Younes <myounes@gmail.com>
2016-02-03 01:08:13 +0200

    2.0.0

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Scroll Lock
 
 Scroll Lock is a jQuery plugin that fully addresses the issue of locking mouse wheel scroll inside a given container, preventing it from propagating to parent element.
 
-> View [demo](http://mohammadyounes.github.io/jquery-scrollLock/example/)
+> View [demo](http://mohammadyounes.github.io/jquery-scrollLock/#demo)
 
 ## Features
 
@@ -16,39 +16,63 @@ Scroll Lock is a jQuery plugin that fully addresses the issue of locking mouse w
 
   > On Windows it can be set to one screen or one line up to 100 lines per notch.
 
-## Usage
+### Install with [NuGet](https://www.nuget.org/packages/jquery-scrollLock/) 
 ```
-// Lock
-$('target').scrollLock();
-
-// Unlock
-$('target').scrollLock('off');
+Install-Package jquery-scrollLock
 ```
 
-### Options
-```
-// @toggle: any true value other than 'off' to lock, otherwise unlock
-// @selector: string to filter the descendants of the selected elements
-// @force: forces lock even if element doesn't have a vertical scroll
-$('target').scrollLock(toggle, selector, force);
-```
-
-#### Delegated handler
-```
-// lock all divs inside a given container
-$('#container').scrollLock('on', 'div');
-```
-
-### Install using Bower
+### Install with Bower
 ```
 bower install jquery-scrollLock
 ```
 
-### Install using NuGet
+## Usage
+
+Trigger Scroll Lock via JavaScript: 
+
 ```
-PM> Install-Package jquery-scrollLock
+$('#target').scrollLock();
+
+```
+
+Trigger Scroll Lock via Markup:
+```
+<!-- HTML -->
+<div data-scrollLock="true" data-strict="true" data-selector=".child" > ... </div>
+
+<!-- JavaScript -->
+$('[data-scrollLock="true"]').scrollLock()
+```
+
+### Options
+
+|   Name    |   Type    | Default   |   Description
+|:----------|:---------:|:---------:|:-------------
+| selector  | `string`  | `false`   | When provided, matching elements will be locked.
+| strict    | `boolean` | `false`   | When enabled, element will be locked even if it has no vertical scrollbar.
+
+### Methods
+
+|   Method                     |    Description
+|:-----------------------------|:--------------
+| `.scrollLock('enable')`      | Enables an element's Scroll Lock.
+| `.scrollLock('disable')`     | Disables an element's Scroll Lock.
+| `.scrollLock('toggleStrict')`| Toggles an element's strict option.
+| `.scrollLock('destroy')`     | Disables and destroys an element's Scroll Lock.
+
+
+### Events
+
+|   Type              |   Description
+|:--------------------|:-------------
+| `top.scrollLock`    | This event fires immediately when the top edge scroll is locked.
+| `bottom.scrollLock` | This event fires immediately when the bottom edge scroll is locked.
+
+```
+$('#target').on('top.scrollLock', function (evt) {
+  // do magic :)
+})
 ```
 
 ------
-
 Have a suggestion or a bug ? please [open a new issue.](https://github.com/MohammadYounes/jquery-scrollLock/issues?state=open)
--- jquery-scrollLock.js
+++ jquery-scrollLock.js
@@ -1,74 +1,99 @@
 /*!
- * Scroll Lock v1.1.1
+ * Scroll Lock v2.0.0
  * https://github.com/MohammadYounes/jquery-scrollLock
  *
- * Copyright (c) 2014 Mohammad Younes
+ * Copyright (c) 2016 Mohammad Younes
  * Licensed under the MIT license.
  */
-(function (factory) {
+;(function (factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['jquery'], factory);
+    define(['jquery'], factory)
   } else {
     // Browser globals
-    factory(jQuery);
+    factory(jQuery)
   }
 }(function ($) {
-
-  var eventName = "onmousewheel" in window ? (("ActiveXObject" in window) ? "wheel" : "mousewheel") : "DOMMouseScroll";
-  var eventNamespace = ".scrollLock";
-
-  var old = $.fn.scrollLock;
-  $.fn.scrollLock = function (toggle, selector, force) {
-
-    if (typeof selector !== 'string')
-      selector = null;
-
-    if ((toggle !== undefined && !toggle) || toggle === 'off')
-      return this.each(function () {
-        $(this).off(eventNamespace);
-      });
-    else
-      return this.each(function () {
-        $(this).on(eventName + eventNamespace, selector, function (event) {
-          //allow zooming
-          if (!event.ctrlKey) {
-            var $this = $(this);
-            if (force === true || hasVerticalScroll($this)) {
-              //Support for nested scrollable blocks (see https://github.com/MohammadYounes/jquery-scrollLock/issues/4)
-              event.stopPropagation();
-              var scrollTop = $this.scrollTop(),
-                  scrollHeight = $this.prop('scrollHeight'),
-                  clientHeight = $this.prop('clientHeight'),
-                  delta = event.originalEvent.wheelDelta || (-1 * event.originalEvent.detail) || (-1 * event.originalEvent.deltaY),
-                  deltaY = 0
-              ;
-              if (event.type === "wheel") {
-                var ratio = $this.height() / $(window).height();
-                deltaY = event.originalEvent.deltaY * ratio;
-              }
-              if (delta > 0 && scrollTop + deltaY <= 0 || delta < 0 && scrollTop + deltaY >= scrollHeight - clientHeight) {
-                event.preventDefault();
-                if (deltaY)
-                  $this.scrollTop(scrollTop + deltaY);
-              }
+  'use strict'
+  var wheelEventName = 'onmousewheel' in window ? (('ActiveXObject' in window) ? 'wheel' : 'mousewheel') : 'DOMMouseScroll'
+  var hasVerticalScroll = function ($element) {
+    var clientWidth = $element.prop('clientWidth'),
+      offsetWidth = $element.prop('offsetWidth'),
+      borderRightWidth = parseInt($element.css('border-right-width'), 10),
+      borderLeftWidth = parseInt($element.css('border-left-width'), 10)
+    return clientWidth + borderLeftWidth + borderRightWidth < offsetWidth
+  }
+  var ScrollLock = function ($element, options) {
+    this.$element = $element
+    this.options = $.extend({}, ScrollLock.DEFAULTS, this.$element.data(), options)
+    this.enabled = true
+    this.$element.on(wheelEventName + ScrollLock.NAMESPACE,
+      this.options.selector,
+      $.proxy(function (event) {
+        // allow zooming
+        if (this.enabled && !event.ctrlKey) {
+          var $this = $(event.currentTarget)
+          if (this.options.strict !== true || hasVerticalScroll($this)) {
+            // Support for nested scrollable blocks (see https://github.com/MohammadYounes/jquery-scrollLock/issues/4)
+            event.stopPropagation()
+            var scrollTop = $this.scrollTop(),
+              scrollHeight = $this.prop('scrollHeight'),
+              clientHeight = $this.prop('clientHeight'),
+              delta = event.originalEvent.wheelDelta || (-1 * event.originalEvent.detail) || (-1 * event.originalEvent.deltaY),
+              deltaY = 0
+            if (event.type === 'wheel') {
+              var ratio = $this.height() / $(window).height()
+              deltaY = event.originalEvent.deltaY * ratio
+            }
+            var top
+            if ((top = delta > 0 && scrollTop + deltaY <= 0) || delta < 0 && scrollTop + deltaY >= scrollHeight - clientHeight) {
+              event.preventDefault()
+              if (deltaY)
+                $this.scrollTop(scrollTop + deltaY)
+              $this.trigger($.Event((top ? 'top' : 'bottom') + ScrollLock.NAMESPACE))
             }
           }
-        });
-      });
-  };
-  function hasVerticalScroll($element) {
-    var clientWidth = $element.prop('clientWidth'),
-        offsetWidth = $element.prop('offsetWidth'),
-        borderRightWidth = parseInt($element.css('border-right-width'), 10),
-        borderLeftWidth = parseInt($element.css('border-left-width'), 10)
-    ;
-
-    return clientWidth + borderLeftWidth + borderRightWidth < offsetWidth;
+        }
+      }, this))
   }
+  ScrollLock.NAME = 'ScrollLock'
+  ScrollLock.VERSION = '2.0.0'
+  ScrollLock.NAMESPACE = '.scrollLock'
+  ScrollLock.DEFAULTS = {
+    strict: false,
+    selector: false
+  }
+  ScrollLock.prototype.toggleStrict = function () {
+    this.options.strict = !this.options.strict
+  }
+  ScrollLock.prototype.enable = function () {
+    this.enabled = true
+  }
+  ScrollLock.prototype.disable = function () {
+    this.enabled = false
+  }
+  ScrollLock.prototype.destroy = function () {
+    this.disable()
+    this.$element.off(ScrollLock.NAMESPACE)
+    this.$element = null
+    this.options = null
+  }
+  // plugin defintion.
+  var old = $.fn.scrollLock
+  $.fn.scrollLock = function (option) {
+    return this.each(function () {
+      var $this = $(this)
+      var options = typeof option == 'object' && option
+      var data = $this.data(ScrollLock.NAME)
+      if (!data && 'destroy' === option) return
+      if (!data) $this.data(ScrollLock.NAME, (data = new ScrollLock($this, options)))
+      if (typeof option == 'string') data[option]()
+    })
+  }
+  $.fn.scrollLock.defaults = ScrollLock.DEFAULTS
   // no conflict
   $.fn.scrollLock.noConflict = function () {
-    $.fn.scrollLock = old;
-    return this;
-  };
-}));
+    $.fn.scrollLock = old
+    return this
+  }
+}))
