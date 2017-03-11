85b5d015f456886ae787ec4cb348db75c1c77268
Mohammad Younes <myounes@gmail.com>
2017-03-11 17:49:19 +0200

    Fixes #13

--- jquery-scrollLock.js
+++ jquery-scrollLock.js
@@ -1,5 +1,5 @@
 /*!
- * Scroll Lock v3.0.0
+ * Scroll Lock v3.1.0
  * https://github.com/MohammadYounes/jquery-scrollLock
  *
  * Copyright (c) 2016 Mohammad Younes
@@ -16,11 +16,41 @@
 }(function ($) {
   'use strict'
   var keys = { space: 32, pageup: 33, pagedown: 34, end: 35, home: 36,  up: 38, down: 40 }
+  var processScrollEvent = function (event, $element) {
+    var scrollTop = $element.scrollTop(),
+      scrollHeight = $element.prop('scrollHeight'),
+      clientHeight = $element.prop('clientHeight'),
+      delta = event.originalEvent.wheelDelta || (-1 * event.originalEvent.detail) || (-1 * event.originalEvent.deltaY),
+      deltaY = 0
+    if (event.type === 'wheel') {
+      var ratio = $element.height() / $(window).height()
+      deltaY = event.originalEvent.deltaY * ratio
+    } else if (this.options.touch && event.type === 'touchmove') {
+      delta = event.originalEvent.changedTouches[0].clientY - this.startClientY
+    }
+    var top, prevent
+    prevent = (top = delta > 0 && scrollTop + deltaY <= 0) || delta < 0 && scrollTop + deltaY >= scrollHeight - clientHeight
+    return {prevent: prevent, top: top, scrollTop: scrollTop, deltaY: deltaY }
+  }
+  var processKeyboardEvent = function (event, $element) {
+    var scrollTop = $element.scrollTop(),
+      result = {top: false, bottom: false}
+    result.top = scrollTop === 0 && (event.keyCode === keys.pageup || event.keyCode === keys.home || event.keyCode === keys.up)
+    if (!result.top) {
+      var scrollHeight = $element.prop('scrollHeight'),
+        clientHeight = $element.prop('clientHeight')
+      result.bottom = scrollHeight === scrollTop + clientHeight && (event.keyCode === keys.space || event.keyCode === keys.pagedown || event.keyCode === keys.end || event.keyCode === keys.down)
+    }
+    return result
+  }
   var ScrollLock = function ($element, options) {
     this.$element = $element
     this.options = $.extend({}, ScrollLock.DEFAULTS, this.$element.data(), options)
     this.enabled = true
     this.startClientY = 0
+    if (this.options.unblock) {
+      this.$element.on(ScrollLock.CORE.wheelEventName + ScrollLock.NAMESPACE, this.options.unblock, $.proxy(ScrollLock.CORE.unblockHandler, this))
+    }
     this.$element.on(ScrollLock.CORE.wheelEventName + ScrollLock.NAMESPACE, this.options.selector, $.proxy(ScrollLock.CORE.handler, this))
     if (this.options.touch) {
       this.$element.on('touchstart' + ScrollLock.NAMESPACE, this.options.selector, $.proxy(ScrollLock.CORE.touchHandler, this))
@@ -29,10 +59,14 @@
     if (this.options.keyboard) {
       this.$element.attr('tabindex', this.options.keyboard.tabindex || 0)
       this.$element.on('keydown' + ScrollLock.NAMESPACE, this.options.selector, $.proxy(ScrollLock.CORE.keyboardHandler, this))
+
+      if (this.options.unblock) {
+        this.$element.on('keydown' + ScrollLock.NAMESPACE, this.options.unblock, $.proxy(ScrollLock.CORE.unblockHandler, this))
+      }
     }
   }
   ScrollLock.NAME = 'ScrollLock'
-  ScrollLock.VERSION = '3.0.0'
+  ScrollLock.VERSION = '3.1.0'
   ScrollLock.NAMESPACE = '.scrollLock'
   ScrollLock.ANIMATION_NAMESPACE = ScrollLock.NAMESPACE + '.effect'
   ScrollLock.DEFAULTS = {
@@ -43,7 +77,8 @@
     selector: false,
     animation: false,
     touch: 'ontouchstart' in window,
-    keyboard: false
+    keyboard: false,
+    unblock: false
   }
   ScrollLock.CORE = {
     wheelEventName: 'onwheel' in document.createElement('div') ? 'wheel' : // Modern browsers support "wheel"
@@ -56,6 +91,9 @@
         'oanimationend',
         'animationend'
       ].join(ScrollLock.ANIMATION_NAMESPACE + ' ') + ScrollLock.ANIMATION_NAMESPACE,
+    unblockHandler: function (event) {
+      event.__currentTarget = event.currentTarget
+    },
     handler: function (event) {
       // allow zooming
       if (this.enabled && !event.ctrlKey) {
@@ -63,24 +101,15 @@
         if (this.options.strict !== true || this.options.strictFn($this)) {
           // Support for nested scrollable blocks (see https://github.com/MohammadYounes/jquery-scrollLock/issues/4)
           event.stopPropagation()
-          var scrollTop = $this.scrollTop(),
-            scrollHeight = $this.prop('scrollHeight'),
-            clientHeight = $this.prop('clientHeight'),
-            delta = event.originalEvent.wheelDelta || (-1 * event.originalEvent.detail) || (-1 * event.originalEvent.deltaY),
-            deltaY = 0
-          if (event.type === 'wheel') {
-            var ratio = $this.height() / $(window).height()
-            deltaY = event.originalEvent.deltaY * ratio
-          } else if (this.options.touch && event.type === 'touchmove') {
-            delta = event.originalEvent.changedTouches[0].clientY - this.startClientY
-          }
-          var top
-          if ((top = delta > 0 && scrollTop + deltaY <= 0) || delta < 0 && scrollTop + deltaY >= scrollHeight - clientHeight) {
+          var result = $.proxy(processScrollEvent, this)(event, $this)
+          if (event.__currentTarget)
+            result.prevent &= $.proxy(processScrollEvent, this)(event, $(event.__currentTarget)).prevent
+          if (result.prevent) {
             event.preventDefault()
-            if (deltaY) {
-              $this.scrollTop(scrollTop + deltaY)
+            if (result.deltaY) {
+              $this.scrollTop(result.scrollTop + result.deltaY)
             }
-            var key = top ? 'top' : 'bottom'
+            var key = result.top ? 'top' : 'bottom'
             if (this.options.animation) {
               setTimeout(ScrollLock.CORE.animationHandler.bind(this, $this, key), 0)
             }
@@ -105,25 +134,25 @@
     },
     keyboardHandler: function (event) {
       var $this = $(event.currentTarget),
-        scrollTop = $this.scrollTop()
-
-      if (scrollTop === 0 && (event.keyCode === keys.pageup || event.keyCode === keys.home || event.keyCode === keys.up)) {
+        scrollTop = $this.scrollTop(),
+        result = processKeyboardEvent(event, $this)
+      if (event.__currentTarget) {
+        var result2 = processKeyboardEvent(event, $(event.__currentTarget))
+        result.top &= result2.top
+        result.bottom &= result2.bottom
+      }
+      if (result.top) {
         $this.trigger($.Event('top' + ScrollLock.NAMESPACE))
         if (this.options.animation) {
           setTimeout(ScrollLock.CORE.animationHandler.bind(this, $this, 'top'), 0)
         }
         return false
-      } else {
-        var scrollHeight = $this.prop('scrollHeight'),
-          clientHeight = $this.prop('clientHeight')
-
-        if (scrollHeight === scrollTop + clientHeight && (event.keyCode === keys.space || event.keyCode === keys.pagedown || event.keyCode === keys.end || event.keyCode === keys.down)) {
-          $this.trigger($.Event('bottom' + ScrollLock.NAMESPACE))
-          if (this.options.animation) {
-            setTimeout(ScrollLock.CORE.animationHandler.bind(this, $this, 'bottom'), 0)
-          }
-          return false
+      } else if (result.bottom) {
+        $this.trigger($.Event('bottom' + ScrollLock.NAMESPACE))
+        if (this.options.animation) {
+          setTimeout(ScrollLock.CORE.animationHandler.bind(this, $this, 'bottom'), 0)
         }
+        return false
       }
     }
   }
@@ -160,4 +189,4 @@
     $.fn.scrollLock = old
     return this
   }
-}));
+}))
